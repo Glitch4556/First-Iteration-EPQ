437eacf926ddcde3d1b16e8a8647aac62102444a
Glitch4556 <114438397+Glitch4556@users.noreply.github.com>
2025-11-13 11:47:33 +0000

    Update ECS.hpp
    accidentally cut chatgpt off

--- ECS/ECS.hpp
+++ ECS/ECS.hpp
@@ -27,4 +27,50 @@ public:
 
     virtual ~Behaviour() = default;
 
-    // Called once per frame to update th
+    // Called once per frame to update this object
+    virtual void onUpdate(sf::RenderWindow& window) {}
+
+    // Called once per frame to render this object
+    virtual void onRender(sf::RenderWindow& window) {}
+
+    // Adds a component of type T to this Behaviour and returns a pointer to it
+    template<typename T, typename... Args>
+    T* AddComponent(Args&&... args)
+    {
+        // Create the new component and store it in the components list
+        std::unique_ptr<T> comp = std::make_unique<T>(std::forward<Args>(args)...);
+        T* ptr = comp.get();
+        components.push_back(std::move(comp));
+        return ptr;
+    }
+
+    // Queues a new Behaviour object for creation (added at the end of the frame)
+    static void CreateObject(std::unique_ptr<Behaviour> obj);
+
+    // Renders all active objects in the scene
+    static void RenderAll(sf::RenderWindow& window);
+
+    // Updates all active objects and handles window events
+    static void UpdateAll(sf::RenderWindow& window);
+
+    // Calls onUpdate for all attached components, then for the Behaviour itself
+    void Update(sf::RenderWindow& window)
+    {
+        for (auto& c : components)
+        {
+            if (c->active)
+                c->onUpdate();
+        }
+        onUpdate(window);
+    }
+
+    // Calls onRender for this Behaviour
+    void Render(sf::RenderWindow& window)
+    {
+        onRender(window);
+    }
+
+private:
+    // List of components attached to this Behaviour
+    std::vector<std::unique_ptr<Component>> components;
+};
